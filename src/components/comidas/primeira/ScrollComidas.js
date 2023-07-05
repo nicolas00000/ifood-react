f6905ceab3bc445957002836be7984853153ec99
nicolas00000 <nicmariano10@gmail.com>
2023-07-05 12:11:23 -0300

    slected item and cont

--- src/components/comidas/primeira/ScrollComidas.js
+++ src/components/comidas/primeira/ScrollComidas.js
@@ -1,19 +1,40 @@
-import React from "react"
+import React, { useState } from "react"
 import Card from './card'
 import './styles.css';
+
 export default function ScrollComidas(){
+    const [boxNumber , setBoxNumber] = useState(null)
+    const [clicado , setClicado] = useState(false)
+    const handleCardClick = (id) => {
+        setBoxNumber(id)
+    };
+    
+
+    const cards = [
+        {nome: "feijao ", preco: "14,00", descricao: "lorem alguma coisa escruta como descircao", img:"alt"},
+        {nome: "arroz ", preco: "14,00", descricao: "lorem alguma coisa escruta como descircao", img:"alt"},   
+        {nome: "carne ", preco: "14,00", descricao: "lorem alguma coisa escruta como descircao", img:"alt"},
+        {nome: "batata ", preco: "14,00", descricao: "lorem alguma coisa escruta como descircao", img:"alt"},
+        {nome: "feijao ", preco: "14,00", descricao: "lorem alguma coisa escruta como descircao", img:"alt"},
+        {nome: "feijao ", preco: "14,00", descricao: "lorem alguma coisa escruta como descircao", img:"alt"},
+    ]
+
     return(
         <div className=" scroll w-full h-80  flex flex-wrap flex-col items-center justify-center overflow-x-scroll">
-            
-            <Card nome="feijao tropeiro" preco="14,00" descricao="lorem alguma coisa escrita como descricao" img="alt"/>
-            <Card nome="Virada paulista" preco="12,30" descricao="lorem alguma coisa escrita como descricao" img="alt"/>
-            <Card nome="Arroz com nozes" preco="22,00" descricao="lorem alguma coisa escrita como descricao" img="alt"/>
-            <Card nome="Bife acebolada" preco="10,00" descricao="lorem alguma coisa escrita como descricao" img="alt"/>
-            <Card nome="feijao" preco="10" descricao="lorem alguma coisa escrita como descricao" img="alt"/>
-            <Card nome="feijao" preco="10" descricao="lorem alguma coisa escrita como descricao" img="alt"/>
-            <Card nome="feijao" preco="10" descricao="lorem alguma coisa escrita como descricao" img="alt"/>
+            {cards.map((card, index) => (
+                <Card 
+                    id={index} // id fixo do card
+                    setarQualCardFoiOClick={() => handleCardClick(index)}
+                    isSelected={setBoxNumber}
+                    nome={card.nome} 
+                    descricao={card.descricao}
+                    preco={card.preco}
+                    boxNumber={boxNumber} //id do card clicado compartilhado com todos 
+                />
+            ))}
 
         </div>
     )
 }
+
 
--- src/components/comidas/primeira/card.js
+++ src/components/comidas/primeira/card.js
@@ -1,18 +1,37 @@
+import React, { useState } from "react"
 import './styles.css'
 export default function Card(props){
-    const {nome, img, preco, descricao} = props 
-    return(
-        <div className=" rounded-md w-44 h-56 inline-block p-3 m-3   bg-white shadow-md cursor-pointer clicado">
+    const {nome, preco, descricao, id, setarQualCardFoiOClick, boxNumber} = props 
+    const [clicado , setClicado] = useState(null)
+    const [quantidade, setQuantidade] = useState(1)
+
+    function selecionado(){
+        // setarQualCardFoiOClick(id)
+        setClicado(true)
+    }
+
+    function add(){
+        setQuantidade(quantidade+1)
+    }
+
+    function remove(){
+        setQuantidade(quantidade-1)
+    }
+
+    return(                                                         //para selecionar apenas 1 ${boxNumber === id ? "border-green-700" : ""}
+        <div onClick={() => selecionado()} className={`${clicado  ? "border-green-700" : ""} relative rounded-md w-44 h-56 inline-block p-3 m-3  border-2  bg-white shadow-md cursor-pointer ` } >
+            {/* {boxNumber} id {id} */}
+            
             <img className="h-20 w-full object-cover rounded " src="https://img.freepik.com/fotos-gratis/closeup-de-carne-assada-com-molho-legumes-e-batatas-fritas-em-um-prato-sobre-a-mesa_181624-35847.jpg?size=626&ext=jpg"></img>
             <strong>{nome}</strong>
             <br></br>
             <desc> {descricao} </desc>
             <div className="flex flex-row justify-between">
                 <strong> R$: {preco} </strong>
-                <div className="flex flex-row w-12 bg-white justify-between">
-                    <p>-</p>
-                    <p>0</p>
-                    <p>+</p>
+                <div className={`${clicado  ? "" : "hidden"} flex flex-row w-16 h-12 bg-gray-400 justify-between p-3  z-10 `}>
+                    <p onClick={remove}>-</p>
+                    <p>{quantidade}</p>
+                    <p className="text-amber-500" onClick={add}>+</p>
                 </div>
             </div>
         </div>
